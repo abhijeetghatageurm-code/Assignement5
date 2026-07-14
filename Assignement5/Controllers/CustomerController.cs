1caef4cf966ce89b75577c5747cd82a2e0f1df98
Administrator <abhijeet.ghatage.urm@gmail.com>
2026-07-14 18:25:42 +0530

    Add project files.

---
+++ Assignement5/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignement5.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+
+
+        [HttpGet]
+        public IActionResult GetCustomer()
+        {
+            var customer = new[]
+            {
+                new{
+                Id = 1,
+                Name = "John Doe",
+                Email = "john.doe@cust.com"
+                },
+                new{
+                Id = 2,
+                Name = "ram teri",
+                Email = "ram.t@cust.com"
+                }
+            };
+
+            return Ok(customer);
+        }
+    }
+}
---
+++ Assignement5/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignement5.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Application is healthy");
+        }
+    }
+}
